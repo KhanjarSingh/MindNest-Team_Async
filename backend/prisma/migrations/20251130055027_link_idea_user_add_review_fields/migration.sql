88e7df2da43a105d65beb9942de0222ee4b75597
Arohi-jd <arohijadhav172@gmail.com>
2025-11-30 11:34:46 +0530

    changed schema

---
+++ backend/prisma/migrations/20251130055027_link_idea_user_add_review_fields/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Idea` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Idea` ADD COLUMN `fundingAmount` INTEGER NULL,
+    ADD COLUMN `note` TEXT NULL,
+    ADD COLUMN `score` INTEGER NULL,
+    ADD COLUMN `status` VARCHAR(191) NOT NULL DEFAULT 'under_review',
+    ADD COLUMN `tags` VARCHAR(191) NULL,
+    ADD COLUMN `userId` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `Idea` ADD CONSTRAINT `Idea_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
