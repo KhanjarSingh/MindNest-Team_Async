b22886a2dd7b5a01f80fefe8083b0237f3305c1e
Jay <jaypatil.nst@gmail.com>
2025-12-01 12:07:28 +0530

    Update Benefits.jsx

--- frontend/client/src/components/Benefits.jsx
+++ frontend/client/src/components/Benefits.jsx
@@ -51,7 +51,7 @@ const Benefits = () => {
         </div>
 
         {/* Benefits Grid */}
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8" style={{gridAutoRows: '1fr'}}>
           {benefits.map((benefit, index) => {
             const Icon = benefit.icon;
             return (
